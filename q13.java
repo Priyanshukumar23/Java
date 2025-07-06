d00c94ab0843ddec03fd3d5fdf262db8cce1cc49
Priyanshu kumar <152582789+Priyanshukumar23@users.noreply.github.com>
2025-07-06 11:23:27 +0530

    Add files via upload

---
+++ q13.java
@@ -0,0 +1,11 @@
+
+	// Online Free Java compiler to run Java program online
+
+	public class Main {
+		public static void main(String[] args) {
+			double celsius = 25;
+            double f = (celsius*9/5)+32;
+            System.out.println(celsius + " Celsius is equal to " + f + " Fahrenheit.");
+
+		}
+	}
