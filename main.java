d712831cca48afda286aec44aa2d360b94851d0a
Priyanshu kumar <152582789+Priyanshukumar23@users.noreply.github.com>
2025-07-06 11:13:08 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int mark1 = 80;
+        int mark2 = 90;
+        int mark3 = 85;
+        float average = (mark1 + mark2 + mark3) / 3.0f;
+        System.out.println("Average marks: " + average);
+    }
+}
