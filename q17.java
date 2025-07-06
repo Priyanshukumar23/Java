4a9e944d06f962c40bdd0e8a841d9ea5dc60ceea
Priyanshu kumar <152582789+Priyanshukumar23@users.noreply.github.com>
2025-07-06 14:19:38 +0530

    Add files via upload

---
+++ q17.java
@@ -0,0 +1,10 @@
+
+	// Online Free Java compiler to run Java program online
+
+	public class Main {
+		public static void main(String[] args) {
+			for(int i=1;i<=10;i++) {
+                System.out.println(i);
+            }
+		}
+	}
