c7310d9b6b76ca90b9afabe79092c4eaf863e5ed
Priyanshu kumar <152582789+Priyanshukumar23@users.noreply.github.com>
2025-07-13 12:02:32 +0530

    Add files via upload

---
+++ 23.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        // Using hardcoded values for testing
+        int num1 = 20;
+        int num2 = 4;
+
+        int sum = num1 + num2;
+        int diff = num1 - num2;
+        int product = num1 * num2;
+        float quotient = (float) num1 / num2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + diff);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+
+        int mark1 = 85, mark2 = 90, mark3 = 95;
+        float average = (mark1 + mark2 + mark3) / 3.0f;
+
+        System.out.println("Average marks: " + average);
+    }
+}
