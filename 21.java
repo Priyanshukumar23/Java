7a2e56bc16f07b7b1d32010cb6c4311ac6e66879
Priyanshu kumar <152582789+Priyanshukumar23@users.noreply.github.com>
2025-07-13 10:58:12 +0530

    Add files via upload

---
+++ 21.java
@@ -0,0 +1,11 @@
+public class Main{
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        String name = "Dhruv";          
+        int age = 34;                   
+        String favoriteLang = "Java";   
+        System.out.printf("My name is %s, I am %d years old, and my favorite programming language is %s.%n",
+                          name, age, favoriteLang);
+    }
+}
